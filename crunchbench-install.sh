3b285e98afc2ed8edd5ba2bc1f32b3f3d0d804a9
Computer Crunch <100100087+computercrunchnz@users.noreply.github.com>
2024-02-09 22:31:35 +1300

    Add files via upload

---
+++ crunchbench-install.sh
@@ -0,0 +1,6 @@
+mkdir crunchbench
+cd crunchbench
+wget https://raw.githubusercontent.com/computercrunchnz/PyBench/172a890233199a444135295d601bdf673a731504/pybench.py
+wget https://raw.githubusercontent.com/computercrunchnz/CrunchBench/main/crunchbench.sh
+wget https://raw.githubusercontent.com/computercrunchnz/CrunchBench/main/crunchbench-run.sh
+sudo apt install sysbench glmark2 python3
